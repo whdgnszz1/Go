db34c394813f0d189e399a00c4f88759a89c15b2
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-01-30 22:31:40 +0900

    Get values from user using fmt.Scan, pointer

--- Basics/investment_calculator.go
+++ Basics/investment_calculator.go
@@ -6,10 +6,26 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	years := 10.0
+	const inflationRate float64 = 2.5
+	var investmentAmount float64
+	var years float64
 	expectedReturnRate := 5.5 
+
+	fmt.Print("Enter investment amount: ")
+	// pointer
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Enter years: ")
+	fmt.Scan(&years)
+
+
 	  
-	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate/100), years) 
+	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate/100), years)
+	futureRealValue := futureValue / math.Pow((1 + inflationRate/100), years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
