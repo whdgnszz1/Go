08ed9949bc6d503277699e4f27bf0304becc9ba5
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-01-30 22:05:06 +0900

    Type overwriting

--- Basics/investment_calculator.go
+++ Basics/investment_calculator.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
+	var investmentAmount float64 = 1000
 	var expectedReturnRate = 5.5
-	var years = 10
+	var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow((1 + expectedReturnRate/100), float64(years)) 
+	var futureValue = investmentAmount * math.Pow((1 + expectedReturnRate/100), years) 
 	fmt.Println(futureValue)
 }
