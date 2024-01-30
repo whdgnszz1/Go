f1aff9695fb01e95d195ab2c9cbf850f2d91b2a0
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-01-30 22:12:56 +0900

    Alternative variable declaration

--- Basics/investment_calculator.go
+++ Basics/investment_calculator.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
-
-	var futureValue = investmentAmount * math.Pow((1 + expectedReturnRate/100), years) 
+	years := 10.0
+	expectedReturnRate := 5.5 
+	  
+	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate/100), years) 
 	fmt.Println(futureValue)
 }
