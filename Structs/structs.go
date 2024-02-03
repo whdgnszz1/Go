e85328d767d667f94eef7651bdb10114442690a4
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-02-03 22:00:38 +0900

    Exporting methods

--- Structs/structs.go
+++ Structs/structs.go
@@ -1,57 +1,28 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
-
-type User struct {
-	firstName string
-	lastName string
-	birthDate string
-	createdAt time.Time
-}
-
-func (u *User) outputUserDetails() {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-}
-
-func (u *User) clearUserName(){
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthdate string) (*User, error) {
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("All fields are required")
-	}
-
-	return &User {
-		firstName: firstName,
-		lastName: lastName,
-		birthDate: birthdate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birth date (MM/DD/YYYY): ")
 
-	var appUser *User
+	var appUser *user.User
 
-	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+	appUser, err := user.New(userFirstName, userLastName, userBirthdate)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	appUser.outputUserDetails()
-	appUser.clearUserName()
-	appUser.outputUserDetails()
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
 }
 
 
---
+++ Structs/user/user.go
@@ -0,0 +1,36 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName string
+	birthDate string
+	createdAt time.Time
+}
+
+func (u *User) OutputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
+func (u *User) ClearUserName(){
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func New(firstName, lastName, birthdate string) (*User, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("All fields are required")
+	}
+
+	return &User {
+		firstName: firstName,
+		lastName: lastName,
+		birthDate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
